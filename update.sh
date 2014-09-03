9fc00c6203e5a785817fe62711f33bde7962fa05
taka <saw@computer.org>
2014-09-03 18:43:25 +0900

    improved updater

--- update.sh
+++ update.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
 PATH="$PATH:/bin/:/usr/bin:/usr/local/bin:"
-DIR="../../data"
+DIR="/home/nowcast250/data"
+OUT_DIR="${DIR}/recent"
 
-PREV_05_FILE="`pwd`/`ls -lt $DIR/*_NOWC_GPV_Ggis0p25km_Prr05lv_Aper5min_FH0000-0030_grib2.bin | head -n 1 | gawk '{print $9}'`"
-PREV_60_FILE="`pwd`/`ls -lt ../../data/*_NOWC_GPV_Ggis0p25km_Pri60lv_Aper5min_FH0000-0030_grib2.bin | head -n 1 | gawk '{print $9}'`"
+# Clean up the old bin files
+cd $OUT_DIR
+PREV_05_FILE="`ls -lt ./*_NOWC_GPV_Ggis0p25km_Prr05lv_Aper5min_FH0000-0030_grib2.bin | head -n 1 | gawk '{print $9}'`"
+PREV_60_FILE="`ls -lt ./*_NOWC_GPV_Ggis0p25km_Pri60lv_Aper5min_FH0000-0030_grib2.bin | head -n 1 | gawk '{print $9}'`"
 if [ -f ${PREV_05_FILE} ]; then
     rm $PREV_05_FILE
 fi
@@ -11,19 +14,37 @@ if [ -f ${PREV_60_FILE} ]; then
     rm $PREV_60_FILE
 fi
 
-LATEST_05_ARCHIVE_FILE="`pwd`/`ls -lt $DIR/*_NOWC_GPV_Ggis0p25km_Prr05lv_Aper5min_FH0000-0030_grib2.bin.gz | head -n 1 | gawk '{print $9}'`"
-LATEST_60_ARCHIVE_FILE="`pwd`/`ls -lt $DIR/*_NOWC_GPV_Ggis0p25km_Pri60lv_Aper5min_FH0000-0030_grib2.bin.gz | head -n 1 | gawk '{print $9}'`"
-gzip -d $LATEST_05_ARCHIVE_FILE
-gzip -d $LATEST_60_ARCHIVE_FILE
+# Find the latest gz file
+cd $DIR
+LATEST_05_ARCHIVE_FILE="`ls -lt ./*_NOWC_GPV_Ggis0p25km_Prr05lv_Aper5min_FH0000-0030_grib2.bin.gz | head -n 1 | gawk '{print $9}'`"
+LATEST_60_ARCHIVE_FILE="`ls -lt ./*_NOWC_GPV_Ggis0p25km_Pri60lv_Aper5min_FH0000-0030_grib2.bin.gz | head -n 1 | gawk '{print $9}'`"
 
-LATEST_05_FILE="`pwd`/`ls -lt $DIR/*_NOWC_GPV_Ggis0p25km_Prr05lv_Aper5min_FH0000-0030_grib2.bin | head -n 1 | gawk '{print $9}'`"
-LATEST_60_FILE="`pwd`/`ls -lt $DIR/*_NOWC_GPV_Ggis0p25km_Pri60lv_Aper5min_FH0000-0030_grib2.bin | head -n 1 | gawk '{print $9}'`"
+# Extract the lates bin files
+LATEST_05_FILE="`echo ${LATEST_05_ARCHIVE_FILE} | sed 's/\.gz//'`"
+LATEST_60_FILE="`echo ${LATEST_60_ARCHIVE_FILE} | sed 's/\.gz//'`"
+if [ ! -f $OUT_DIR/$LATEST_05_FILE ]; then
+        gzip -c -d $DIR/$LATEST_05_ARCHIVE_FILE > $OUT_DIR/$LATEST_05_FILE
+fi
+if [ ! -f $OUT_DIR/$LATEST_60_FILE ]; then
+        gzip -c -d $DIR/$LATEST_60_ARCHIVE_FILE > $OUT_DIR/$LATEST_60_FILE
+fi
 
-echo "processing ${LATEST_05_FILE}"
+# Parse the nowcast data
+cd $OUT_DIR
 TIME="`echo ${LATEST_05_FILE} | sed 's/.*RJTD_//' | sed 's/00_.*//'`"
-wgrib2 ${LATEST_05_FILE} -csv_nz nowcast_${TIME}_05_1.csv 1.0 1>/dev/null
-echo "processing ${LATEST_60_FILE}"
+CSV_05_FILE="nowcast_${TIME}_05_1.csv"
+if [ ! -f $OUT_DIR/$CSV_05_FILE ]; then
+        echo "processing ${LATEST_05_FILE}"
+        wgrib2 ${LATEST_05_FILE} -csv_nz ${CSV_05_FILE} 1.0 1>/dev/null
+fi
 TIME="`echo ${LATEST_60_FILE} | sed 's/.*RJTD_//' | sed 's/00_.*//'`"
-wgrib2 ${LATEST_60_FILE} -csv_nz nowcast_${TIME}_60_1.csv 1.0 1>/dev/null
+CSV_60_FILE="nowcast_${TIME}_60_1.csv"
+if [ ! -f $OUT_DIR/$CSV_60_FILE ]; then
+        echo "processing ${LATEST_60_FILE}"
+        wgrib2 ${LATEST_60_FILE} -csv_nz ${CSV_60_FILE}  1.0 1>/dev/null
+fi
+
+# Clean up the old csv files
+find $OUT_DIR -mtime +1 -name "*.csv"  | xargs rm
 
 
